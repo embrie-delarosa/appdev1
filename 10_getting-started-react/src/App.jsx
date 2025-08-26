c794eab6a9387593d4e152be1a1751d126af7393
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-08-26 12:48:41 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <h2>Name: Embrie A. Dela Rosa</h2>
+      <h3>Course & Year: Bachelor of Science in Information Systems - 3rd Year</h3>
+      <p>Fun Fact: The first hobby I learned was playing billiards.</p>
+      <p>
+        I want to learn React because it makes building interactive and dynamic web applications easier. 
+        It will also help me improve my skills in frontend development and prepare for real-world projects.
+      </p>
+    </div>
+  );
+}
+
+export default App;
