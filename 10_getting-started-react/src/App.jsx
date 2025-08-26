2a72d15ab05548d0082104964293b26611adb148
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-08-26 12:51:43 +0800

    Activity 10: Add React About Me project

--- 10_getting-started-react/src/App.jsx
+++ 10_getting-started-react/src/App.jsx
@@ -2,9 +2,9 @@ function App() {
   return (
     <div>
       <h1>About Me</h1>
-      <h2>Name: Embrie A. Dela Rosa</h2>
-      <h3>Course & Year: Bachelor of Science in Information Systems - 3rd Year</h3>
-      <p>Fun Fact: The first hobby I learned was playing billiards.</p>
+      <h2>Name: Embrie Dela Rosa</h2>
+      <h3>Course & Year: BS Information Technology 3rd Year</h3>
+      <p>Fun Fact: I have a twin sister from BS Accountancy 3rd Year.</p>
       <p>
         I want to learn React because it makes building interactive and dynamic web applications easier. 
         It will also help me improve my skills in frontend development and prepare for real-world projects.
