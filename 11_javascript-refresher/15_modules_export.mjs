ca071697841c496354af2f39955ae8c6b863ff2b
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-09-06 10:58:30 +0800

    Add modules export activity

---
+++ 11_javascript-refresher/15_modules_export.mjs
@@ -0,0 +1,11 @@
+// Default export: a function
+export default function greet() {
+  return "Hello from my module in 15th file!";
+}
+
+// Named export: your info
+export const info = {
+  name: "Embrie",
+  age: 20,
+  mod: "This message is from 15_modules_export.mjs"
+};
