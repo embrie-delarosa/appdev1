c04caf4a02f50a079fc1afbdd57524cae61fbbc9
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-09-16 22:17:16 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -45,6 +45,7 @@ function AboutPage() {
     <>
       <h1>About</h1>
       <p>Hello there.<br />How do you do?</p>
+      <img className="avatar" src={reactLogo} style={{ borderRadius: '50%' }} />
     </>
   )
 
