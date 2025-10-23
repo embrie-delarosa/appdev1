bf6261735393fd05579c9156a3bfa64d3b55e72c
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-10-23 21:01:48 +0800

    Done with how to use curly braces with JSX to access JavaScript functionality from your components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Gallery from './Gallery.jsx';
 import LamarrToDo from './LamarrToDo.jsx';
+import ZaraToDo from './ZaraToDo.jsx';
 
 export default function App() {
   return (
     <div className="App">
       <Gallery />
       <LamarrToDo />
+      <ZaraToDo />
     </div>
   );
 }
