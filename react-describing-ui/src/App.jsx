18940ca797f0961fe9bdf8fcbdb46998382db60b
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-10-23 20:59:07 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Gallery from './Gallery.jsx';
+import LamarrToDo from './LamarrToDo.jsx';
 
 export default function App() {
   return (
     <div className="App">
       <Gallery />
+      <LamarrToDo />
     </div>
   );
 }
