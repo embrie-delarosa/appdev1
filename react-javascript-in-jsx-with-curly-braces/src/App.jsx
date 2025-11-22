c764628679cfc7fdc5aca168d2fb8d08b1bd981f
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-11-22 14:33:46 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -32,6 +32,16 @@ export default function TodoList() {
       <Avatar />
       <TodoList1 />
       <h1>To Do List for {formatDate(today)}</h1>
+      <ul style={
+        {
+          backgroundColor: 'black',
+          color: 'pink'
+        }
+      }>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
     </>
   );
 }
