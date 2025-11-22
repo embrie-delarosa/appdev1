7b1205f2bb73a8c4d0e318a70a484e67211fa93e
embriedelarosa <embriedelarosa@student.laverdad.edu.ph>
2025-11-22 14:32:01 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,4 +1,4 @@
-export default function Avatar() {
+export function Avatar() {
   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
   const description = 'Gregorio Y. Zara';
   return (
@@ -9,3 +9,30 @@ export default function Avatar() {
     />
   );
 }
+
+export function TodoList1() {
+  const name = 'Gregorio Y. Zara';
+  return (
+    <h1>{name}'s To Do List</h1>
+  );
+}
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
+  return (
+    <>
+      <Avatar />
+      <TodoList1 />
+      <h1>To Do List for {formatDate(today)}</h1>
+    </>
+  );
+}
+
